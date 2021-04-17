f14553ec495e2174f03af94851d46fe37ac9b819
Thomas Dubiel <td1729@hotmail.com>
2021-04-17 06:03:06 -0500

    add Navbar component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,10 @@
 import React from 'react'; 
+import Navbar from './components/Navbar';
 
 const App =  () => { 
     return ( 
         <div> 
+            <Navbar />
             <h1 align="center">Dubiel Digital</h1>
             <h2 align="center">Full Software Project Builds From Scratch</h2>
         </div>
---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'; 
+import { NavLink } from 'react-router-dom'
+
+
+function Navbar() { 
+    return( 
+        <h1>Navbar</h1>
+    )
+ 
+} 
+
+export default Navbar; 
