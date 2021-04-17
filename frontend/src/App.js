702d8c4dd1b7d5b18120874dc1743154a651444c
Thomas Dubiel <td1729@hotmail.com>
2021-04-17 06:13:05 -0500

    create Footer component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react'; 
-import Navbar from './components/Navbar';
+import Navbar from './components/Navbar'; 
+import Footer from './components/Footer';
 
 const App =  () => { 
     return ( 
         <div> 
             <Navbar />
             <h1 align="center">Dubiel Digital</h1>
-            <h2 align="center">Full Software Project Builds From Scratch</h2>
+            <h2 align="center">Full Software Project Builds From Scratch</h2> 
+            <br /> 
+            <Footer />
         </div>
     )
 } 
---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,8 @@
+import React from 'react'; 
+
+function Footer() { 
+    return( 
+        <h3>Footer</h3>
+    )
+} 
+export default Footer;
